0e0441ea9c15773511d9fd92ab87048d6b7ecbbf
Berglind Brá Jóhannsdóttir <berglindbrajohannsdottir@Berglinds-MacBook-Pro.local>
2020-08-28 16:05:31 +0200

    endurröðun typo posters

--- src/specifics/Typoposters.svelte
+++ src/specifics/Typoposters.svelte
@@ -6,12 +6,12 @@
 <div class="backgroundcolor">
 	<br><br>
 	<img class="img portfolio-item" src="igms/typoPosters/3.jpg" style="" /><br>
-	<img class="img portfolio-item" src="igms/typoPosters/7.jpg" style="" /><br>
 	<img class="img portfolio-item" src="igms/typoPosters/4.jpg" style="" /><br>
 	<img class="img portfolio-item" src="igms/typoPosters/puffwind2.png" /><br>
 	<img class="img portfolio-item" src="igms/typoPosters/1.jpg" style="" /><br>
 	<!--<img class="img portfolio-item" src="igms/typoPosters/8.jpg" style="" />-->
 	<img class="img portfolio-item smaller" src="igms/typoPosters/arial2.png" /><br>
+	<img class="img portfolio-item" src="igms/typoPosters/7.jpg" style="" /><br>
 	<img class="img portfolio-item" src="igms/typoPosters/5.jpg" style="" /><br>
 	<img class="img portfolio-item" src="igms/typoPosters/2.jpg" style="" /><br>
 	<br><br><br><br><br>
